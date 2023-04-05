b077937bc48acfe540d4815287210c1cae03b203
Douglas Hubler <douglas@hubler.us>
2023-04-05 09:10:12 -0400

    update gnmi among other things

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/freeconf/examples
 go 1.18
 
 require (
-	github.com/freeconf/restconf v0.0.0-20230325123108-f8e243b22ea0
-	github.com/freeconf/yang v0.0.0-20230402092844-505d4a4f35c6
+	github.com/freeconf/restconf v0.0.0-20230405122357-35c2d910ad38
+	github.com/freeconf/yang v0.0.0-20230405122240-93e4375862dd
+)
+
+require (
+	github.com/freeconf/gnmi v0.0.0-20230405122604-b473842e6f13 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/openconfig/gnmi v0.9.1 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230323212658-478b75c54725 // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
--- openconfig-gnmic/test-commands.sh
+++ openconfig-gnmic/test-commands.sh
@@ -2,23 +2,35 @@
 
 set -euf -o pipefail
 
-# Get Data : full path
+# Get Data Object
 gnmic --config car.yml get --path car:/
 
-# Get Data : use model
-gnmic --config car.yml get --model car --path ""
+# Get Data Value
+gnmic --config car.yml get --path car:/speed
 
-# Set Data
+# Get Data : use model.
+gnmic --config car.yml get --model car --path /
+
+
+# Set Data Object
 gnmic --config car.yml set --update  car:/:::json:::'{"speed":300}'
+
+# Set Data Value
+gnmic --config car.yml set --update-path  car:/speed --update-value 300
+
 
 # Subscribe
 timeout 5s \
-  gnmic --config car.yml sub --model car --path "" --sample-interval 1s --heartbeat-interval 2s || true
+  gnmic --config car.yml sub --model car --path / --sample-interval 1s --heartbeat-interval 2s || true
 
 # Subscribe to just tire metrics : use model
 timeout 5s \
-  gnmic --config car.yml sub --mode once --model car --path "/tire" || true
+  gnmic --config car.yml sub --mode once --model car --path /tire || true
 
-# Subscribe to just tire metrics : full path
+# Subscribe to just tire metrics
 timeout 5s \
-  gnmic --config car.yml sub --mode once --path "car:/tire" || true
+  gnmic --config car.yml sub --mode once --path car:/tire || true
+
+# Subscribe to just tire 1 metrics
+timeout 5s \
+  gnmic --config car.yml sub --mode once --path car:/tire=1 || true
